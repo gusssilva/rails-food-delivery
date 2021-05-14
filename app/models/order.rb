e69daf3992ff4851d38cd052118d58f6c513001f
Gustavo Silva <guss.silva@hotmail.com>
2021-05-14 18:08:43 -0300

    orders controller and views all set & running

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,16 @@ class Order < ApplicationRecord
   validates :name, :phone_number, :total_value, :city, :neighborhood, :street, :number, presence: true
 
   enum status: { waiting: 0, delivered: 1 }
+  before_validations :set_price
+  accepts_nested_attributes_for :order_products, allow_destroy: true
+
+  private
+
+  def set_price
+    final_price = 0
+    order.order_products.each do |order_product|
+      final_price += order_product.quantity * op.product.price
+    end
+    self.total_value = final_price + self.restaurant.delivey_tax
+  end
 end
